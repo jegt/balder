2bcb4f0720d630fd9cc66bf64a32e66f097c8560
Jonas Tehler <jegt@jegt.net>
2010-04-02 18:54:53 +0200

    Support for incoming emails

---
+++ app/controllers/incoming_controller.rb
@@ -0,0 +1,12 @@
+class IncomingController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token
+  
+  def index
+    logger.debug "REMOTE IP: "+request.remote_ip.to_s
+    if params[:mail] && request.remote_ip == APP_CONFIG[:mail_server_ip]
+      Mailer.receive(params[:mail])
+    end
+  end
+
+end
